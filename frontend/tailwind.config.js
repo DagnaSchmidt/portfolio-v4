caca3525c8c1406ef68d5a82a48a4fef9e56163f
Dagna Schmidt <dagdagi889@gmail.com>
2024-01-19 11:34:47 +0100

    tailwind config theme colors extend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#803F3A',
+        secondary: '#F0EAD8'
+      }
+    },
   },
   plugins: [],
 }
-
