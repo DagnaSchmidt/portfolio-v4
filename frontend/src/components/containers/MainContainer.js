085554ca6b1748c64bad3226afa69e99112cdb49
Dagna Schmidt <dagdagi889@gmail.com>
2024-01-19 14:48:23 +0100

    main container - part 1

--- frontend/src/components/containers/MainContainer.js
+++ frontend/src/components/containers/MainContainer.js
@@ -1,8 +1,64 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const MainContainer = () => {
+    const dispatch = useDispatch();
+    const page = useSelector(state => state.pages.page);
+
+    const variants = {
+        close: {
+            width: '128px',
+            height: '1px'
+        },
+        open: {
+            width: '100dvw',
+            height: '100dvh'
+        }
+    }
+
+
   return (
-    <div>MainContainer</div>
+    <div>
+
+        <AnimatePresence>
+            {
+                page === 'home' &&
+
+                    <motion.div>
+                        <button>
+                            <p className='text-primary text-sm amulya-medium hover:amulya-bold'>
+                                Work
+                            </p>
+                        </button>
+                    </motion.div>
+            }
+        </AnimatePresence>
+
+        <motion.div
+            className='bg-primary'
+            variants={variants}
+            initial='close'
+            animate={page === 'home' ? 'close' : 'open'}
+        >
+
+        </motion.div>
+
+        <AnimatePresence>
+            {
+                page === 'home' &&
+
+                    <motion.div>
+                        <button>
+                            <p className='text-primary text-sm amulya-medium hover:amulya-bold'>
+                                About
+                            </p>
+                        </button>
+                    </motion.div>
+            }
+        </AnimatePresence>
+
+    </div>
   )
 }
 
