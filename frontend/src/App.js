e37a083015f8a19cad7b971b4a94ab5031d25b7d
Dagna Schmidt <dagdagi889@gmail.com>
2024-01-19 12:06:23 +0100

    updated index and app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import './styles/index.css';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-      </Routes>
+
     </>
   );
 }
