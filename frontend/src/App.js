774e9e489f35c099261bc8c4f5068be99eb1e5e3
Dagna Schmidt <dagdagi889@gmail.com>
2024-01-19 12:12:18 +0100

    updated app, added main container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './styles/index.css';
 
+//components
+import MainContainer from './components/containers/MainContainer';
+
+
 function App() {
   return (
-    <>
-
-    </>
+    <div>
+      <MainContainer />
+    </div>
   );
 }
 
--- frontend/src/components/Home/HomeComponentExample.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HomeComponentExample = () => {
-  return (
-    <div>HomeComponentExample</div>
-  )
-}
-
-export default HomeComponentExample;
