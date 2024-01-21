9022e2428af7586e22a04ba43d836e1618f16aaf
Dagna Schmidt <dagdagi889@gmail.com>
2024-01-21 11:40:58 +0100

    about page - part 2

--- frontend/src/components/about/AboutPage.js
+++ frontend/src/components/about/AboutPage.js
@@ -6,11 +6,12 @@ import Link from '../shared/Link';
 
 //data
 import { aboutLinks } from '../../data/aboutLinks';
+import { aboutData } from '../../data/aboutData';
 
 const AboutPage = () => {
   return (
     <motion.div
-      className='w-full max-w-[1480px] h-full px-2 md:px-8 xl:px-24 pt-6 flex flex-col'
+      className='w-full max-w-[1480px] h-full px-2 md:px-8 xl:px-56 pt-6 flex flex-col gap-10 md:gap-20 xl:gap-40'
       initial={{opacity: 0}}
       animate={{opacity: 1}}
       transition={{
@@ -22,6 +23,16 @@ const AboutPage = () => {
         {
           aboutLinks.map(i => <Link key={i.id} url={i.link} title={i.title} color='text-secondary' />)
         }
+      </div>
+
+      <div className='flex flex-col md:flex-row gap-10'>
+        <div className='flex flex-col gap-3'>
+          <h2 className='amulya-bold text-xl md:text-2xl xl:text-3xl text-secondary tracking-wider'>Hello, I am Dagna</h2>
+          <p className='amulya text-sm text-secondary pl-12'>{aboutData.description}</p>
+        </div>
+        <button className='w-32 self-center md:self-end md:mx-12 xl:mx-24'>
+          <p className='amulya-light text-secondary hover:italic w-32 border-b border-secondary'>download CV</p>
+        </button>
       </div>
     </motion.div>
   )
---
+++ frontend/src/data/aboutData.js
@@ -0,0 +1,10 @@
+export const aboutData = {
+    description: 'and I am a fullstack developer located in Göteborg, Sweden. With a background in Interior Design from Academy of Fine Arts in Warsaw, I bring the unique blend of creativity and technical expertise to my projects. My passion for UI design allowed me to develop an eye for detail that helps me create visually appealing websites and applications.',
+    other: [
+        {
+            id: 1,
+            title: '',
+            link: ''
+        }
+    ]
+}
