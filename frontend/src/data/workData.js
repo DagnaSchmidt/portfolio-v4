d0db514878d93fff7170ba813354636eee4d4036
Dagna Schmidt <dagdagi889@gmail.com>
2024-02-18 15:07:32 +0100

    movies project udpate

--- frontend/src/data/workData.js
+++ frontend/src/data/workData.js
@@ -59,18 +59,28 @@ export const workData = [
         id: 3,
         title: 'Movies4you',
         prevImg: 'prev_movies4you.png',
-        categories: 'App design  ||  UI  ||  Design System',
+        categories: 'App design  ||  UI  ||  Design System || Frontend || Backend',
         description: "Explore the world of cinema like never before with Movies4You, a stunning mobile app that redefines your movie-watching experience. Navigate effortlessly through a seamless design crafted for a delightful user experience.",
         links: [
             {
                 id: 1,
-                title: 'Prototype',
+                title: 'UI Prototype',
                 link: 'https://www.figma.com/proto/exTv5ZsK0UDgOrQSmRAyQq/1.-M4y?page-id=10%3A2&node-id=152%3A5730&viewport=475%2C-1315%2C0.29&scaling=contain&starting-point-node-id=10%3A18'
             },
             {
                 id: 2,
                 title: 'UI design',
                 link: 'https://dribbble.com/shots/23009539-Movies4you-UI'
+            },
+            {
+                id: 3,
+                title: 'Demo',
+                link: 'https://movies-dagnas-projects.vercel.app/'
+            },
+            {
+                id: 4,
+                title: 'GitHub repo',
+                link: 'https://github.com/DagnaSchmidt/movies'
             }
         ],
         buildOn: [
@@ -78,6 +88,16 @@ export const workData = [
                 id: 1,
                 title: 'UI',
                 stack: ['Figma']
+            },
+            {
+                id: 2,
+                title: 'Frontend',
+                stack: ['React', 'TypeScript', 'Axios', 'Tailwind']
+            },
+            {
+                id: 3,
+                title: 'Backend',
+                stack: ['REST API', 'MongoDB']
             }
         ]
     },
